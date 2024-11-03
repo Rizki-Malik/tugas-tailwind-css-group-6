ba286461fdb7bbeda04e328f64eb0daaa4e35024
Ky0ma <rimali.qwerty@gmail.com>
2024-11-03 13:58:02 +0700

    fixed : content html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,html}",
     "./index.html",
   ],
   theme: {
